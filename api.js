80467fd924d2d3718183a89ab96f55512db4ebd7
Henrik Uran Nykmark <henya004@osloskolen.no>
2023-09-22 10:47:53 +0200

    first commit

---
+++ api.js
@@ -0,0 +1,20 @@
+let out = ""
+let ut = document.getElementById("out")
+let checkmark
+
+function klikk(){
+ fetch("https://jsonplaceholder.typicode.com/todos/10")
+	 .then(respons => {
+		 out += respons.status
+		 return respons.json()
+	 })
+	 .then(data => {
+		 checkmark = data.completed ? "☑":"☐"
+		 out += "<br>"+checkmark+" "+data.title
+	 })
+	
+ ut.innerHTML = out
+ out = "hei"
+}
+
+ut.textContent = out
