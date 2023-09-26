f30770e30e312d7c2648afb47f1892faca546e9c
Henrik Uran Nykmark <henye004@osloskolen.no>
2023-09-26 10:11:49 +0200

    fiksa kode så nå funker det

--- api.js
+++ api.js
@@ -6,12 +6,12 @@ let valuttaUt = "USD"
 function klikk(){
  fetch("https://v6.exchangerate-api.com/v6/07e664c29b9b2782755ddd2d/latest/" +valuttaInn)
 	 .then(respons => {
-		 out += respons.status
+		 console.log(respons.status)
 		 return respons.json()
 	 })
 	 .then(data => {
 		let amount = parseFloat(document.getElementById("amount").value)
-		let text = amount+" "+valuttaInn+ "="
+		let text = amount+" "+valuttaInn+ " = "
 		let conversion = data.conversion_rates[valuttaUt] 
 		text += (conversion*amount) + valuttaUt
 		out.textContent = text
