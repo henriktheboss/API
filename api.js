ce2b7248b07d8e9beecc81206e7622838aa0bb30
Henrik Uran Nykmark <henye004@osloskolen.no>
2023-09-26 08:21:11 +0200

    lagde input felt

--- api.js
+++ api.js
@@ -1,20 +1,25 @@
 let out = ""
 let ut = document.getElementById("out")
+let inn = document.getElementById("inn")
 
 
 function klikk(){
  fetch("https://v6.exchangerate-api.com/v6/07e664c29b9b2782755ddd2d/latest/NOK")
 	 .then(respons => {
-		 out += respons.status
+		 //out += respons.status
 		 return respons.json()
 	 })
 	 .then(data => {
 	
-		 out += "<br> "+data.result
+		 
+		 out += inn.value
+		 out += "<br>" +(data.conversion_rates.USD * inn.value)
+
+
 	 })
 	
  ut.innerHTML = out
  out = ""
 }
 
-ut.textContent = out
+ut.value = out 
