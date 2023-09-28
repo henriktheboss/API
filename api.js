2d0b28c5b08e4766898940ed43507ee0cc62a55d
Henrik Uran Nykmark <henye004@osloskolen.no>
2023-09-28 11:23:26 +0200

    lå til drop down bar for bade penger ut og inn men inn funker ikke

--- api.js
+++ api.js
@@ -1,19 +1,22 @@
 let out = document.getElementById("out")
-let valuttaInn = "NOK"
-let valuttaUt = "USD"
+let valutar = document.getElementById("valutar")
+let inval = document.getElementById("inval")
+
 
 
 function klikk(){
- fetch("https://v6.exchangerate-api.com/v6/07e664c29b9b2782755ddd2d/latest/" +valuttaInn)
+	let pengin = inval.value;
+ fetch("https://v6.exchangerate-api.com/v6/07e664c29b9b2782755ddd2d/latest/" +pengin)
 	 .then(respons => {
 		 console.log(respons.status)
 		 return respons.json()
 	 })
 	 .then(data => {
 		let amount = parseFloat(document.getElementById("amount").value)
-		let text = amount+" "+valuttaInn+ " = "
-		let conversion = data.conversion_rates[valuttaUt] 
-		text += (conversion*amount) + valuttaUt
+		
+		let valgtPeng = valutar.value;
+		let conversion = data.conversion_rates[pengin] 
+		let text = (conversion*amount) + valgtPeng
 		out.textContent = text
 
 
