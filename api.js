35f08da7849e157855653d82d0a1837aacc1bd15
Henrik Uran Nykmark <henye004@osloskolen.no>
2023-09-29 08:45:24 +0200

    lå til feil melding hvis du ikke har skrid noe

--- api.js
+++ api.js
@@ -1,27 +1,28 @@
-let out = document.getElementById("out")
-let valutar = document.getElementById("valutar")
-let inval = document.getElementById("inval")
+let out = document.getElementById("out");
+let valutar = document.getElementById("valutar");
+let inval = document.getElementById("inval");
 
-
-
-function klikk(){
-	let pengin = inval.value;
- fetch("https://v6.exchangerate-api.com/v6/07e664c29b9b2782755ddd2d/latest/" +pengin)
-	 .then(respons => {
-		 console.log(respons.status)
-		 return respons.json()
-	 })
-	 .then(data => {
-		let amount = parseFloat(document.getElementById("amount").value)
-		
-		let valgtPeng = valutar.value;
-		let conversion = data.conversion_rates[valutar.value] 
-		let text = ""+(conversion*amount) + valgtPeng
-		out.textContent = text
-
-
-	 })
-	
-
+function klikk() {
+    let pengin = inval.value;
+    
+   
+    let amountField = document.getElementById("amount");
+    if (amountField.value === "") {
+        out.textContent = "Skriv inn tall";
+        return; 
+    }
+    
+    let amount = parseFloat(amountField.value);
+    let valgtPeng = valutar.value;
+    
+    fetch("https://v6.exchangerate-api.com/v6/07e664c29b9b2782755ddd2d/latest/" + pengin)
+        .then(response => {
+            console.log(response.status);
+            return response.json();
+        })
+        .then(data => {
+            let conversion = data.conversion_rates[valutar.value];
+            let text = "" + (conversion * amount) + valgtPeng;
+            out.textContent = text;
+        });
 }
-
